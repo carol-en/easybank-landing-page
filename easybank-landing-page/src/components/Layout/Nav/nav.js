bdead4afc0a1c9beca322f9658bf07bfcc6de301
Caroline Nolasco <cnolascoix@gmail.com>
2020-03-18 11:55:38 -0700

    wip nav mobile styling

--- easybank-landing-page/src/components/Layout/Nav/nav.js
+++ easybank-landing-page/src/components/Layout/Nav/nav.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import Logo from "./images/logo.svg";
+import CloseIcon from "./images/icon-close.svg";
 import "./nav.scss";
 
 class Nav extends Component {
   render() {
     return(
-      <>
-      <h1>Nav Component</h1>
-      Home
-      About
-      Contact
-      Blog
-      Careers
+      <nav>
+      <button className="logo-link">
+        <img src={Logo} alt="Easy Bank"/>
+        <img src={CloseIcon} alt="Close Navigation"/>
+      </button>
+      <a  className="nav-link" href="#">Home</a>
+      <a  className="nav-link" href="#">About</a>
+      <a  className="nav-link" href="#">Contact</a>
+      <a  className="nav-link" href="#">Blog</a>
+      <a  className="nav-link" href="#">Careers</a>
 
-      Request Invite
-      </>
+      <button>Request Invite</button>
+      </nav>
     )
   }
 }
