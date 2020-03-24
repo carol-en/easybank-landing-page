6d747f4c59c54645c5e00b14541a1eb6bf034c53
Caroline Nolasco <cnolascoix@gmail.com>
2020-03-24 12:14:48 -0700

    set initial state for nav media queries

--- easybank-landing-page/src/components/Layout/Nav/nav.js
+++ easybank-landing-page/src/components/Layout/Nav/nav.js
@@ -11,11 +11,28 @@ class Nav extends Component {
   }
 
   componentDidMount() {
-    this.setState({ navOpen: false });
+    // this.setState({ navOpen: false });
+    this.setLayout();
   }
 
+  setLayout = () => {
+    window.addEventListener("resize", () => {
+      const mobile = window.innerWidth < 900;
+      const desktop = window.innerWidth > 900;
+  
+      if(mobile) {
+        this.setState({ navOpen: false });
+      } else if(desktop) {
+        this.setState({ navOpen: true });
+      }
+    });
+  }
+ 
   toggleMobileNav = () => { 
-   this.setState({ navOpen:!this.state.navOpen });
+    const mobile = window.innerWidth < 900;
+    if(mobile) {
+      this.setState({ navOpen:!this.state.navOpen });
+    }
   }
 
   render() {
@@ -27,15 +44,15 @@ class Nav extends Component {
     return(
       <nav onClick={this.toggleMobileNav}>
         <button className="logo-link">
-          <img src={Logo} alt="Easy Bank"/>
-          <img src={CloseIcon} alt="Close Navigation"/>
+          <img className="bank-logo" src={Logo} alt="Easy Bank"/>
+          <img className="close-x" src={CloseIcon} alt="Close Navigation"/>
         </button>
         
-        <a  className={navLink} href="#">Home</a>
-        <a  className={navLink} href="#">About</a>
-        <a  className={navLink} href="#">Contact</a>
-        <a  className={navLink} href="#">Blog</a>
-        <a  className={navLink} href="#">Careers</a>
+        <a  className={navLink} href="https://google.com">Home</a>
+        <a  className={navLink} href="https://google.com">About</a>
+        <a  className={navLink} href="https://google.com">Contact</a>
+        <a  className={navLink} href="https://google.com">Blog</a>
+        <a  className={navLink} href="https://google.com">Careers</a>
 
         <button className={gradientBtn}>Request Invite</button>
       </nav>
