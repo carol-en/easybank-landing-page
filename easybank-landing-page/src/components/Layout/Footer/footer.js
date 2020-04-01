ed695871d65480eff7908fe0dc822a1ee4f97727
Caroline Nolasco <cnolascoix@gmail.com>
2020-04-01 16:04:08 -0700

    wip footer icons

--- easybank-landing-page/src/components/Layout/Footer/footer.js
+++ easybank-landing-page/src/components/Layout/Footer/footer.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import "./footer.scss";
-import { logo } from "./images";
-
+import iconFacebook from "./images/icon-facebook.svg";
+import iconInstagram from "./images/icon-instagram.svg";
+import iconPinterest from "./images/icon-pinterest.svg";
+import iconTwitter from "./images/icon-twitter.svg";
+import iconYoutube from "./images/icon-youtube.svg";
 class Footer extends Component {
   render() {
     return(
       <footer>
         <span className="logo" />
         <aside className="social-medias">
-          <a href="/"><img src={`${logo}.svg`} /></a>
-          <a href="/"></a>
-          <a href="/"></a>
-          <a href="/"></a>
-          <a href="/"></a>
+          <a href="https://facebook.com"><img src={iconFacebook} alt="Facebook" /></a>
+          <a href="https://youtube.com"><img src={iconYoutube} alt="Youtube"/></a>
+          <a href="https://twitter.com"><img src={iconTwitter} alt="Twitter"/></a>
+          <a href="https://pinterest.com"><img src={iconPinterest} alt="Pinterest"/></a>
+          <a href="https://instagram.com"><img src={iconInstagram} alt="Instagram"/></a>
         </aside>
 
         {/* https://www.freecodecamp.org/forum/t/importing-images-in-react/206974/3 */}
