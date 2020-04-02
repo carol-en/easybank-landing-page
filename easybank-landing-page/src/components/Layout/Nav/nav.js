220976ec44ba46ff8a9340762037af137f46f131
Caroline Nolasco <cnolascoix@gmail.com>
2020-04-02 11:18:42 -0700

    finished refactor nav styling

--- easybank-landing-page/src/components/Layout/Nav/nav.js
+++ easybank-landing-page/src/components/Layout/Nav/nav.js
@@ -36,25 +36,25 @@ class Nav extends Component {
   render() {
     const { navOpen } = this.state;
     const active= navOpen && "active";
-    const wrap = `wrap ${active}`;
     const openX = `open-x ${navOpen && "close-x"}`;
 
     return(
-      <nav onClick={this.toggleMobileNav}>
-        <button className="logo-link">
-          <span className="bank-logo" />
-          <span className={openX} />
-        </button>
+      <header>
+          <button className="logo-link" onClick={this.toggleMobileNav}>
+            <span className="bank-logo" />
+            <span className={openX} />
+          </button>
 
-        <div className={wrap}>
-          <a  className="nav-link" href="https://google.com">Home</a>
-          <a  className="nav-link" href="https://google.com">About</a>
-          <a  className="nav-link" href="https://google.com">Contact</a>
-          <a  className="nav-link" href="https://google.com">Blog</a>
-          <a  className="nav-link" href="https://google.com">Careers</a>
-        </div>
-
-      </nav>
+          <nav className={active}>
+            <div className="wrap">
+              <a  className="nav-link" href="https://google.com">Home</a>
+              <a  className="nav-link" href="https://google.com">About</a>
+              <a  className="nav-link" href="https://google.com">Contact</a>
+              <a  className="nav-link" href="https://google.com">Blog</a>
+              <a  className="nav-link" href="https://google.com">Careers</a>
+            </div>
+          </nav>
+      </header>
     )
   }
 }
