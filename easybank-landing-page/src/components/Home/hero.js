861dd7aa7f23aa487b16d7ca25d3ba1c5b07fb82
Caroline Nolasco <cnolascoix@gmail.com>
2020-04-03 11:43:32 -0700

    wip hero desktop

--- easybank-landing-page/src/components/Home/hero.js
+++ easybank-landing-page/src/components/Home/hero.js
@@ -5,18 +5,20 @@ class Hero extends Component {
   render() {
     return(
       <section className="hero">
-        <header className="hero-images">
-          <aside className="inner-header"></aside>
-        </header>
-        <aside className="blurb">
+        <main className="container">
+          <aside className="hero-images">
+            <div className="inner-header"></div>
+          </aside>
 
-          <h1 className="lead">Next generation digital banking</h1>
-          <p>
-            Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
-          </p>
-          <button className="gradient-btn">Request Invite</button>
-
-        </aside>
+        
+          <aside className="blurb">
+            <h1 className="lead">Next generation digital banking</h1>
+            <p>
+              Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
+            </p>
+            <button className="gradient-btn">Request Invite</button>
+          </aside>
+        </main>
       </section>
     )
   }
