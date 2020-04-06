71319bb9a2ea085d9d3ca9f131fe98f01fa7e4c1
Caroline Nolasco <cnolascoix@gmail.com>
2020-04-06 11:14:10 -0700

    finished desktop css article and whys desktop

--- easybank-landing-page/src/components/Home/articles.js
+++ easybank-landing-page/src/components/Home/articles.js
@@ -5,85 +5,87 @@ class Articles extends Component {
   render() {
     return(
       <section className="articles">
-        
-          <h1 className="lead-articles">Latest Articles</h1>
+        <main className="container">
+            <h1 className="lead-articles">Latest Articles</h1>
 
-          <article className="featured-article">
-           <a href="/">
-            <aside className="article-hero">
-                  <img src="https://i.imgur.com/7QhsNZ9.jpg" alt="Receive money in any currency with no fees" />
-            </aside>
+            <article className="featured-article">
+            <a href="/">
+              <aside className="article-hero">
+                    <img src="https://i.imgur.com/7QhsNZ9.jpg" alt="Receive money in any currency with no fees" />
+              </aside>
 
-            <aside className="article-content">
-              <span className="author">By Claire Robinson</span>
-                <h2 className="article-title">
-                    Receive money in any currency with no fees
-                </h2>
-              <p>
-                The world is getting smaller and we’re becoming more mobile. So why should you be 
-                forced to only receive money in a single …
-              </p>
-            </aside>
+              <aside className="article-content">
+                <span className="author">By Claire Robinson</span>
+                  <h2 className="article-title">
+                      Receive money in any currency with no fees
+                  </h2>
+                <p>
+                  The world is getting smaller and we’re becoming more mobile. So why should you be 
+                  forced to only receive money in a single …
+                </p>
+              </aside>
+              </a>
+            </article>
+
+          
+            <article className="featured-article">
+            <a href="/">
+              <aside className="article-hero">
+                  <img src="https://i.imgur.com/W3Dd8YE.jpg" alt="Treat yourself without worrying about money" />
+              </aside>
+              
+              <aside className="article-content">
+                <span className="author">By Wilson Hutton</span>
+                  <h2 className="article-title">
+                      Treat yourself without worrying about money
+                  </h2>
+                <p>
+                  Our simple budgeting feature allows you to separate out your spending and set 
+                  realistic limits each month. That means you   …
+                </p>
+              </aside>
             </a>
-          </article>
+            </article>
 
-         
-          <article className="featured-article">
-           <a href="/">
-            <aside className="article-hero">
-                <img src="https://i.imgur.com/W3Dd8YE.jpg" alt="Treat yourself without worrying about money" />
-            </aside>
-            
-            <aside className="article-content">
-              <span className="author">By Wilson Hutton</span>
-                <h2 className="article-title">
-                    Treat yourself without worrying about money
-                </h2>
-              <p>
-                Our simple budgeting feature allows you to separate out your spending and set 
-                realistic limits each month. That means you   …
-              </p>
-            </aside>
-           </a>
-          </article>
+            <article className="featured-article">
+            <a href="/">
+              <aside className="article-hero">
+                    <img src="https://i.imgur.com/WTuels2.jpg" alt="Take your Easybank card wherever you go" />
+              </aside>
 
-          <article className="featured-article">
-           <a href="/">
-            <aside className="article-hero">
-                  <img src="https://i.imgur.com/WTuels2.jpg" alt="Take your Easybank card wherever you go" />
-            </aside>
-
-            <aside className="article-content">
-              <span className="author">By Wilson Hutton</span>
-                <h2 className="article-title">
-                    Take your Easybank card wherever you go
-                </h2>
-              <p>
-                We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
-                while you’re abroad. We’ll even show you …
-              </p>
-            </aside>
-           </a>
-          </article>
-
-          <article className="featured-article">
-           <a href="/">
-            <aside className="article-hero">
-                  <img src="https://i.imgur.com/PdPfHuA.jpghttps://i.imgur.com/PdPfHuA.jpg" alt="Our invite-only Beta accounts are now live!" />
-            </aside>
-
-            <aside className="article-content">
-              <span className="author">By Claire Robinson</span>
-                <h2 className="article-title">
-                    Our invite-only Beta accounts are now live!
-                </h2>
-              <p>
-                After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
-                It’s easy to request an invite through the site ...
-              </p>
-            </aside>
+              <aside className="article-content">
+                <span className="author">By Wilson Hutton</span>
+                  <h2 className="article-title">
+                      Take your Easybank card wherever you go
+                  </h2>
+                <p>
+                  We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+                  while you’re abroad. We’ll even show you …
+                </p>
+              </aside>
             </a>
-          </article>
+            </article>
+
+            <article className="featured-article">
+            <a href="/">
+              <aside className="article-hero">
+                    <img src="https://i.imgur.com/PdPfHuA.jpghttps://i.imgur.com/PdPfHuA.jpg" alt="Our invite-only Beta accounts are now live!" />
+              </aside>
+
+              <aside className="article-content">
+                <span className="author">By Claire Robinson</span>
+                  <h2 className="article-title">
+                      Our invite-only Beta accounts are now live!
+                  </h2>
+                <p>
+                  After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+                  It’s easy to request an invite through the site ...
+                </p>
+              </aside>
+              </a>
+            </article>
+
+          </main>
       </section>
     )
   }
--- easybank-landing-page/src/components/Home/why.js
+++ easybank-landing-page/src/components/Home/why.js
@@ -5,40 +5,41 @@ class Why extends Component {
   render() {
     return(
       <section className="why">
+        <main className="container">
         <aside className="why-main">
           <h2 className="why-lead">Why choose Easybank?</h2>
           <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
           your finances like never before.</p>
         </aside>
 
-        <aside>
+        <aside className="why-cards">
           <span className="choose-icons online-banking"></span>
           <h3 className="why-title">Online Banking</h3>
           <p>Our modern web and mobile applications allow you to keep track of your finances 
           wherever you are in the world.</p>
         </aside>
 
-        <aside>
+        <aside className="why-cards">
           <span className="choose-icons simple-budgeting"></span>
           <h3 className="why-title">Simple Budgeting</h3>
           <p>See exactly where your money goes each month. Receive notifications when you’re 
           close to hitting your limits.</p>
         </aside>
 
-        <aside>
+        <aside className="why-cards">
           <span className="choose-icons fast-onboarding"></span>
           <h3 className="why-title">Fast Onboarding</h3>
           <p>We don’t do branches. Open your account in minutes online and start taking control 
           of your finances right away.</p>
         </aside>
 
-        <aside>
+        <aside className="why-cards">
           <span className="choose-icons open-api"></span>
           <h3 className="why-title">Open API</h3>
           <p>Manage your savings, investments, pension, and much more from one account. Tracking 
           your money has never been easier.</p>
         </aside>
-
+        </main>
       </section>
     )
   }
