2cbd12a93f90db84e295c8e77b8af2861defea75
Caroline Nolasco <cnolascoix@gmail.com>
2020-04-09 17:10:27 -0700

    footer styling desktop finished

--- easybank-landing-page/src/components/Layout/Footer/footer.js
+++ easybank-landing-page/src/components/Layout/Footer/footer.js
@@ -9,27 +9,34 @@ class Footer extends Component {
   render() {
     return(
       <footer>
-        <span className="logo" />
-        <aside className="social-medias">
-          <a href="https://facebook.com"><img src={iconFacebook} alt="Facebook" /></a>
-          <a href="https://youtube.com"><img src={iconYoutube} alt="Youtube"/></a>
-          <a href="https://twitter.com"><img src={iconTwitter} alt="Twitter"/></a>
-          <a href="https://pinterest.com"><img src={iconPinterest} alt="Pinterest"/></a>
-          <a href="https://instagram.com"><img src={iconInstagram} alt="Instagram"/></a>
-        </aside>
+        <main className="container">
+          <aside className="social-medias">
+            <span className="logo" />
+            
+            <span className="socials">
+              <a href="https://facebook.com"><img src={iconFacebook} alt="Facebook" /></a>
+              <a href="https://youtube.com"><img src={iconYoutube} alt="Youtube"/></a>
+              <a href="https://twitter.com"><img src={iconTwitter} alt="Twitter"/></a>
+              <a href="https://pinterest.com"><img src={iconPinterest} alt="Pinterest"/></a>
+              <a href="https://instagram.com"><img src={iconInstagram} alt="Instagram"/></a>
+            </span>
+          </aside>
 
 
-        <ul className="site-map">
-          <li><a href="/">About Us</a></li>
-          <li><a href="/">Contact</a></li>
-          <li><a href="/">Blog</a></li>
-          <li><a href="/">Careers</a></li>
-          <li><a href="/">Support</a></li>
-          <li><a href="/">Privacy Policy</a></li>
-        </ul>
+          <ul className="site-map">
+            <li><a href="/">About Us</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Careers</a></li>
+            <li><a href="/">Support</a></li>
+            <li><a href="/">Privacy Policy</a></li>
+          </ul>
 
-        <button className="gradient-btn">Request Invite</button>
-          <p className="copyright">© Easybank. All Rights Reserved</p>
+          <aside className="book-end">
+            <button className="gradient-btn">Request Invite</button>
+            <p className="copyright">© Easybank. All Rights Reserved</p>
+          </aside>
+        </main>
       </footer>
     )
   }
